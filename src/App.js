03bdceb1c77d8aa3cf95f36f81ba44dd076b2fbb
Will McGIll <willmcgill@=.localdomain>
2019-10-17 13:14:51 -0400

    created working clock

--- src/App.js
+++ src/App.js
@@ -1,13 +1,43 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+// import ClockDisplay from './ClockDisplay.js'
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+
+class App extends React.Component {
+
+    constructor(props){
+        super(props);
+        this.state = {date: new Date()};
+    }
+componentDidMount(){
+    this.timerID = setInterval(
+        () => this.tick(),
+        1000
+    );
+}
+componentWillUnmount(){
+
+}
+
+tick() {
+    this.setState({
+      date: new Date()
+    });
+  }
+
+    render(){
+        return(
+            <div className = "App">
+            <h1>Hello, world</h1>
+            <h2>It is {this.state.date.toLocaleTimeString()}</h2>
+            </div>
+        )
+    }
+    
+    //setInterval(App, 1000)
+    
+    
 }
 
 export default App;
---
+++ src/ClockDisplay.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class Clockdisplay extends React.Component{
+    render(){
+        return{
+            
+        }
+    }
+}
+
+export default Clock;
+
+
+
+
+
+
+
+
